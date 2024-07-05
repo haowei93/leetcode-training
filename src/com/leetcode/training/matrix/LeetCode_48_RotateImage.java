149ae0ac2d4195234a02f417e54aa416181b7cee
harryhook <chenhaowei93@163.com>
2024-07-05 22:23:41 +0800

    反转矩阵

--- src/com/leetcode/training/matrix/LeetCode_48_RotateImage.java
+++ src/com/leetcode/training/matrix/LeetCode_48_RotateImage.java
@@ -6,12 +6,35 @@ public class LeetCode_48_RotateImage {
 
         LeetCode_48_RotateImage client = new LeetCode_48_RotateImage();
         int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        client.rotate(matrix);
+        client.rotate1(matrix);
         for (int[] row : matrix) {
             for (int num : row) {
                 System.out.print(num + " ");
             }
             System.out.println();
+        }
+
+    }
+
+    public void rotate1(int[][] matrix) {
+        int n = matrix.length;
+
+        // 行翻转
+        for (int i = 0; i < n/2; i++) {
+            for (int j = 0; j < n ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-i-1][j];
+                matrix[n-i-1][j] = temp;
+            }
+        }
+
+        // 主对角线翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
         }
 
     }
